03232d8a0e82ed760b1186e3a2a6911b1fba6e5a
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-10-06 16:17:10 +0530

    Updates

---
+++ precache-manifest.cb80d32d8d2de7b4867cc134e617a45c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391b283e9c87fb808b82a94d92f11444",
+    "url": "/react-redux-todo-list/index.html"
+  },
+  {
+    "revision": "2327f621c67109c829c8",
+    "url": "/react-redux-todo-list/static/js/2.13f33e61.chunk.js"
+  },
+  {
+    "revision": "a132b71a3fc5d9b13ed7",
+    "url": "/react-redux-todo-list/static/js/main.4adc4927.chunk.js"
+  },
+  {
+    "revision": "88a9e51e5b9b17dcaadd",
+    "url": "/react-redux-todo-list/static/js/runtime-main.0e5d4622.js"
+  }
+]);
